59acb7d1d5b4afa532d82d273d9c82346c4ea59d
Tianle <tianle.ni@tum.de>
2024-02-05 14:12:46 +0100

    1

--- competition/flexibleTrajectoryPlanner/onlinelocalReplanner.py
+++ competition/flexibleTrajectoryPlanner/onlinelocalReplanner.py
@@ -9,7 +9,7 @@ import scipy.optimize as opt
 import math
 import matplotlib.pyplot as plt
 
-VERBOSE_PLOT = True
+VERBOSE_PLOT = False
 VMAX = 6
 AMAX = 2  # 3 is risky in simple but works in hard and standard
 LAMBDA_GATES = 4000
